19513c3f40f2417d655408e69c98f45347b3b6e7
rou-bai <2802607350@qq.com>
2021-04-05 20:37:59 +0800

    商品详情增加redis缓存

--- livegoods_details/src/main/java/com/livegoods/houses/LivegoodsHousesApplication.java
+++ livegoods_details/src/main/java/com/livegoods/houses/LivegoodsHousesApplication.java
@@ -2,8 +2,10 @@ package com.livegoods.houses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LivegoodsHousesApplication {
     public static void main(String[] args){
         SpringApplication.run(LivegoodsHousesApplication.class, args);
--- livegoods_details/src/main/java/com/livegoods/houses/service/impl/HousesServiceImpl.java
+++ livegoods_details/src/main/java/com/livegoods/houses/service/impl/HousesServiceImpl.java
@@ -5,6 +5,7 @@ import com.livegoods.pojo.Houses;
 import com.livegoods.houses.service.HousesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,7 +24,18 @@ public class HousesServiceImpl implements HousesService {
         return housesDao.findAll();
     }
 
+    /**
+     * 增加缓存，redis。
+     * MongoDB是一个高效的NoSQL数据库，查询效率非常高。
+     * 增加缓存的目的，是减少MongoDB压力。减少代码压力。
+     * 虽然Redis效率确实比MongoDB高一点，很多时候，这点性能提升，不足以
+     * 屏蔽缓存的各种问题。如：缓存穿透、雪崩、击穿。
+     * @param id
+     * @return
+     */
+
     @Override
+    @Cacheable(cacheNames = "livegoods:details", key="'getHouseById(' + #id + ')'")
     public Houses getHouseById(String id){
         Houses houses = housesDao.findById(id);
         String[] imgs = houses.getImgs();
